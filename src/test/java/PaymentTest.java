0a28ee4d66cca41889ab9d3396e072f6fc05238e
a.sterkhov <a.sterkhov@tinkoff.ru>
2019-04-22 16:29:28 +0400

    firefox

--- src/test/java/PaymentTest.java
+++ src/test/java/PaymentTest.java
@@ -1,4 +1,6 @@
 import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import pages.MainMenu;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
@@ -7,73 +9,36 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import pages.Payments;
 
+import java.util.concurrent.TimeUnit;
+
 public class PaymentTest {
-    private WebDriver driver;
+
 
     @BeforeSuite
     public void initDriver() throws Exception {
-        MyChromeDriver dr = new MyChromeDriver();
-        driver = dr.getChromeDriver(driver);
+
+
     }
 
     @Test
     public void checkPayment() throws InterruptedException {
-        MainMenu mainMenu = PageFactory.initElements(driver, MainMenu.class);
-        mainMenu.chooseElement("Платежи");
-        Payments payments = PageFactory.initElements(driver, Payments.class);
-        payments.chooseElement("Коммунальные платежи");
-        String currentRegion = payments.getCurrentRegion();
-        if(!currentRegion.equals("г. Москва")){
-            payments.chooseRegion("г. Москва");
-        }
-        payments.openRegionMenu();
-        payments.chooseRegion("г. Москва");
-        String paymentHolder = payments.choosePaymentHolder();
-        payments.clickPayButton();
-
-        payments.fillPaymentPeriod("102017");
-        payments.fillPaymentAmount("100");
-        payments.checkLoan();
-        payments.checkErrorMessage("Поле обязательное");
-
-        payments.fillPayerCode("123");
-        payments.checkLoan();
-        payments.checkErrorMessage("Поле неправильно заполнено");
-
-        payments.fillPayerCode("0000000000");
-        payments.fillPaymentPeriod("999999");
-        payments.checkLoan();
-        payments.checkErrorMessage("Поле заполнено некорректно");
-
-        payments.fillPaymentPeriod("102017");
-        payments.fillPaymentAmount("0");
-        payments.checkLoan();
-        payments.checkErrorMessage("Минимальная сумма перевода - 10 \u20BD");
-
-        mainMenu.chooseElement("Платежи");
-        payments.fillPaymentFilter("жку");
-        String filteredPaymentHolder = payments.getPaymentHolderFiltered(0);
-        if(!filteredPaymentHolder.toLowerCase().contains(paymentHolder.toLowerCase())){
-            throw new IllegalStateException("не совпадает название поставщика услуг: " + filteredPaymentHolder + " " + paymentHolder);
-        }
-        payments.clickPayButton();
-        payments.fillPayerCode("0000000000");
-        payments.fillPaymentPeriod("102017");
-        payments.fillPaymentAmount("0");
-        payments.checkLoan();
-        payments.checkErrorMessage("Минимальная сумма перевода - 10 \u20BD");
-
-        mainMenu.chooseElement("Платежи");
-
-        payments.openRegionMenu();
-        payments.chooseRegion("г. Санкт-Петербург");
-        payments.chooseElement("Коммунальные платежи");
-        payments.scrollToBottom();
-        Assert.assertFalse("На странице найден лишний поставщик: " + paymentHolder, payments.searchPaymentHolder(paymentHolder));
+        System.setProperty("webdriver.gecko.driver","C:\\geckodriver\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://mdmdev.sdi-solution.ru/login");
+        TimeUnit.SECONDS.sleep(10);
+        driver.findElement(By.xpath("//*[contains(@id, 'email')]/parent::div")).click();
+        driver.findElement(By.xpath("//*[contains(@id, 'email')]")).sendKeys("artem");
+        driver.findElement(By.id("password")).sendKeys("111");
+        driver.findElement(By.xpath("//*[contains(text(),'Войти')]")).click();
+        driver.get("http://mdmdev.sdi-solution.ru/content/I?classifierId=4&treeItemId=-1&taxonomyReferenceId=468");
+        //driver.findElement(By.xpath("//*[contains(@title,'Композиты')]")).click();
+        TimeUnit.SECONDS.sleep(5);
+        driver.findElement(By.xpath("(//*[contains(@class,'toggle-children')]/parent::span)[3]")).click();
+        TimeUnit.SECONDS.sleep(5);
     }
 
     @AfterSuite
     public void quitDriver() throws Exception {
-        driver.quit();
+        //driver.quit();
     }
 }
